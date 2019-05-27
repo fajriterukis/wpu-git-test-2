c34d2e342f4a0c7bff0897dbcaee41daabf1ef4d
Muhammad Fajri <mfajri429@gmail.com>
2019-05-27 16:14:17 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,9 @@
 <body>
 	<h1>Hello World!</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis vitae modi eum assumenda rem, aperiam sed quidem distinctio delectus quisquam vero fuga perferendis eaque adipisci sequi facilis ipsam odit eligendi.</p>
+	<ul>
+		<li>satu</li>
+		<li>dua</li>
+	</ul>
 </body>
 </html>
